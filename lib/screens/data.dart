2e8011e33d69c270d954f1eb001f334d3360b6b4
Shaunak <shaunak.warty@gmail.com>
2021-09-12 01:31:24 -0700

    Linkify urls in data

--- lib/screens/data.dart
+++ lib/screens/data.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DataScreen extends StatelessWidget {
   const DataScreen({Key? key, required this.data}) : super(key: key);
 
   final String data;
+
+  void _openURL(BuildContext context, String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: const Text('Failed to launch URL'),
+        action: SnackBarAction(
+          label: 'OK',
+          onPressed: () {},
+        ),
+      ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +31,11 @@ class DataScreen extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            Text(data, style: const TextStyle(fontSize: 20.0)),
+            Linkify(
+              text: data,
+              style: const TextStyle(fontSize: 20.0),
+              onOpen: (link) => _openURL(context, link.url),
+            ),
           ],
         ),
       ),
