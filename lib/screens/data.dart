d4038371623d600a26fdf45e1e5b77e16b8115aa
Shaunak Warty <shaunak.warty@gmail.com>
2021-09-19 19:14:39 -0700

    Add button to copy data to clipboard

--- lib/screens/data.dart
+++ lib/screens/data.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_linkify/flutter_linkify.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -6,6 +7,17 @@ class DataScreen extends StatelessWidget {
   const DataScreen({Key? key, required this.data}) : super(key: key);
 
   final String data;
+
+  void _copyData(BuildContext context) async {
+    await Clipboard.setData(ClipboardData(text: data));
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: const Text('Data copied to clipboard'),
+      action: SnackBarAction(
+        label: 'OK',
+        onPressed: () {},
+      ),
+    ));
+  }
 
   void _openURL(BuildContext context, String url) async {
     if (await canLaunch(url)) {
@@ -26,6 +38,12 @@ class DataScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('New Scan'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.copy),
+            onPressed: () => _copyData(context),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
